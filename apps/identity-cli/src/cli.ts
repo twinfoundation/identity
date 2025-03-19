586b7e6f35a18c9d161c2a7de7384191e33b8b10
Cornel Cristian Filip <filipcornelcristian@gmail.com>
2025-03-19 08:51:54 +0000

    bump version

--- apps/identity-cli/src/cli.ts
+++ apps/identity-cli/src/cli.ts
@@ -40,7 +40,7 @@ export class CLI extends CLIBase {
 			{
 				title: "TWIN Identity",
 				appName: "twin-identity",
-				version: "0.0.1-next.35",
+				version: "0.0.1-next.36",
 				icon: "🌍",
 				supportsEnvFiles: true,
 				overrideOutputWidth: options?.overrideOutputWidth
